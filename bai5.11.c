84f8df147b6d878702e287becea7776ae22e3fb3
Bino <trieuthan92pro@gmail.com>
2019-10-06 12:11:06 +0700

    Add exercises 5.11

--- bai5.11.c
+++ bai5.11.c
@@ -5,11 +5,21 @@ void hienThi(char s[100][20], int size);
 void tachTu(char*);
 
 void tachTu(char* s) {
-	
+	char res[100][20];
+	int i = 0;
+	char *p;
+	const char *delim = " .,-\t\n";
+	for(p = strtok(s, delim); p != NULL; p = strtok(NULL, delim)) {
+		strcpy(res[i++], p);
+	}
+	hienThi(res, i);
 }
 
 void hienThi(char s[100][20], int size) {
-	
+	int i;
+	for(i = size-1; i >= 0; i--) {
+		printf("%s ", s[i]);
+	}
 }
 
 int main(){
