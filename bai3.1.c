bc8006a7364260f3793b406b4b629e335718bc83
unknown <trieuthan92pro@gmail.com>
2020-03-10 23:40:52 +0700

    Update ex 3.1 3.2

--- bai3.1.c
+++ bai3.1.c
@@ -2,20 +2,18 @@
 
 int main() {
 	int m, n;
-	printf("Nhap m, n > 0: ");
+	printf("Nhap hai so duong m, n: ");
 	scanf("%d%d", &m, &n);
-	
 	if(m > 0 && n > 0) {
 		int i, j;
 		for(i = 1; i <= m; i++) {
 			for(j = 1; j <= n; j++) {
-				printf(" x ");
+				printf(" %d ", j);
 			}
 			printf("\n");
 		}
 	} else {
-		printf("Nhap m, n > 0!");
+		printf("Nhap so duong m, n: ");
 	}
-	
 	return 0;
 }
