8fb729f54624d4d977341fcf270e5ae029af3d24
thantrieu <trieuthan92pro@gmail.com>
2020-04-05 17:06:54 +0700

    Update ex 3.13

--- bai3.13.c
+++ bai3.13.c
@@ -2,64 +2,69 @@
 #include <stdbool.h>
 #include <math.h>
 
-bool nguyenTo(int n) {
-	
-}
-
-int nhap() {
-	int n = 0;
+int nhapN() {
+	int n = -1;
 	while(n <= 0) {
-		printf("Nhap n > 0: ");
+		printf("Nhap so nguyen n > 0: ");
 		scanf("%d", &n);
 	}
 	return n;
 }
 
-void lietKeNguyenTo(int n) {
-	printf("\nN so nguyen to dau tien: \n");
-	int count = 0;
+bool ktraNgto(int n) {
+	if(n < 2) {
+		return false;
+	}
+	int i;
+	for(i = 2; i <= sqrt(n); i++) {
+		if(n % i == 0) {
+			return false;
+		}
+	}
+	return true;
+}
+
+void lietKeNgto(int n) {
+	printf("%d so nguyen to dau tien: \n", n);
 	int i = 2;
-	while(count < n) {
-		if(nguyenTo(i)) {
-			printf("%-10d", i);
-			count ++;
+	while(n > 0) {
+		if(ktraNgto(i)) {
+			printf("%d, ", i);
+			n--;
 		}
 		i++;
 	}
 }
 
-void lietKeChinhPhuong(int n) {
-	printf("\nN so chinh phuong dau tien: \n");
+void lietKeFibo(int n) {
+	printf("\n%d so Fibonacci dau tien: \n", n);
+	long long f1 = 1, f0 = 0, fn;
 	int i;
 	for(i = 0; i < n; i++) {
-		printf("%-10d", i * i);
-	}
-}
-
-void lietKeFibonacci(int n) {
-	printf("\nN so fibonacci dau tien: \n");
-	int i;
-	unsigned long long f0 = 0;
-	unsigned long long f1 = 1;
-	unsigned long long fn;
-	for(i = 0; i < n; i++) {
-		if(i < 2) {
+		if(i <= 1) {
 			fn = i;
 		} else {
-			fn = f0 + f1;
+			fn = f1 + f0;
 			f0 = f1;
 			f1 = fn;
 		}
-		printf("F%d = %lld\n", i, fn);
+		printf("F%d = %lld\n", i, fn);		
+	}
+}
+
+void lietKeChinhPhuong(int n) {
+	printf("\n%d so chinh phuong dau tien: \n", n);
+	int i;
+	for(i = 0; i < n; i++) {
+		printf("%d, ", i * i);
 	}
 }
 
 int main() {
-	int n = nhap();
-	lietKeNguyenTo(n);
-	lietKeFibonacci(n);
+	int n = nhapN();
+	lietKeNgto(n);
+	lietKeFibo(n);
 	lietKeChinhPhuong(n);
-	
 	return 0;
 }
 
